c2d6bd415c26a95d55946b56f1b3745580f1576b
AlexLosciale <losciale17@gmail.com>
2025-03-16 09:51:01 +0100

    add:start

---
+++ data/db.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2';
+
+const conncection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+conncection.connect((err) =>{
+    if (err) throw err;
+    console.log('Connessione al DB avvenuta con successo');
+});
+
+export default conncection;
+
